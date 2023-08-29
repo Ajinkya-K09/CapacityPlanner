e52bbe8a002e9f8415f1f50752603d5bc9f96b0a
Ajinkya Kulkarni <ak152122@renishaw.com>
2023-08-29 19:54:06 +0530

    Added backend functionality for the UI

---
+++ Model/VelocityCalculatorModel.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+
+namespace VelocityCalculator.Model
+{
+    public class VelocityCalculatorModel : INotifyPropertyChanged
+    {
+        private int m_numberOfMembers;
+        private int m_comapanyHolidays;
+        private int m_totalLeaves;
+        private int m_totalSprintDays;
+
+        public int NumberOfMembers
+        {
+            get
+            {
+                return m_numberOfMembers;
+            }
+            set
+            {
+                m_numberOfMembers = value;
+            }
+        }
+
+        public int CompnayHolidays
+        {
+            get
+            {
+                return m_numberOfMembers;
+            }
+            set
+            {
+                m_numberOfMembers = value;
+            }
+        }
+
+        public int TotalLeaves
+        {
+            get
+            {
+                return m_numberOfMembers;
+            }
+            set
+            {
+                m_numberOfMembers = value;
+            }
+        }
+
+        public int TotalSprintDays
+        {
+            get
+            {
+                return m_numberOfMembers;
+            }
+            set
+            {
+                m_numberOfMembers = value;
+            }
+        }
+
+        public int TotalWorkingDays { get; set; }
+
+        public bool IsSatSunOff { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ViewModel/VelocityCalculatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using VelocityCalculator.Model;
+
+namespace VelocityCalculator.ViewModel
+{
+    public class VelocityCalculatorViewModel
+    {
+        public VelocityCalculatorViewModel()
+        {
+            var velocityCalculatorModelVM = new VelocityCalculatorModel();
+            VelocityCalculatorVM = velocityCalculatorModelVM;
+        }
+
+        public VelocityCalculatorModel VelocityCalculatorVM { get; set; }
+
+    }
+}
